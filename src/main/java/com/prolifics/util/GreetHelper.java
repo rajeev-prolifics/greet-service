932cec4f4a1860a20f95dddeb5dc0fa4f4aeb455
Rajeev Sharma <rsharma@ad.prolifics.com>
2021-03-22 20:40:07 +0530

    Added some more quotes

--- src/main/java/com/prolifics/util/GreetHelper.java
+++ src/main/java/com/prolifics/util/GreetHelper.java
@@ -5,14 +5,14 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * GreetHelper holds the quotes which will be randomly picked by
- * the API and returned back to the caller.
+ * GreetHelper holds the quotes which will be randomly picked by the API and
+ * returned back to the caller.
  * 
  * @author Prolifics
  *
  */
 public class GreetHelper {
-	
+
 	/**
 	 * The quotes repository.
 	 */
@@ -20,20 +20,21 @@ public class GreetHelper {
 			"The greatest glory in living lies not in never falling, but in rising every time we fall.",
 			"The way to get started is to quit talking and begin doing.",
 			"If life were predictable it would cease to be life, and be without flavor.",
-			"Life is what happens when you're busy making other plans"
-	});
-	
+			"Life is what happens when you're busy making other plans",
+			"Don’t downgrade your dream just to fit your reality, upgrade your conviction to match your destiny.",
+			"You are confined only by the walls you build yourself.",
+			"Stop being afraid of what could go wrong and think of what could go right." });
 
 	/**
-	 * getRandomQuote returns a random quote from the 
-	 * defined repository.
+	 * getRandomQuote returns a random quote from the defined repository.
 	 * 
 	 * @return
-	 * 	<p>The desired Quote.</p>
+	 *         <p>
+	 *         The desired Quote.
+	 *         </p>
 	 */
 	public static String getRandomQuote() {
 		return quotes.get(new Random().nextInt(quotes.size()));
-	}	
-	
+	}
 
 }
